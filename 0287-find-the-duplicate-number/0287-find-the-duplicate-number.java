6d1d69ea6a66da89a19c158948734f4d4ea878d9
Abhinav Ranga <abhinavranga2006@gmail.com>
2026-07-02 05:27:40 +0530

    Time: 4 ms (90.56%), Space: 82.8 MB (57.42%) - LeetHub - Time: O(n), Space: O(1)

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -22,6 +22,16 @@ class Solution {
     }
 }
 
+// in this question the only thing is we are using linked list logic here inside the memory it is still an array but
+// we are travesing it as a linked list  
+// steps
+// when u are at any index then the value itself will behave like index and u are not traversing here contiguously 
+// inside the memory it is still contiguous array but we are not traversing it like normal array 
+
+// so in linked list the node that is being pointed  by two nodes  means it is the starting of the cycle means 
+// that particular node is coming again (repeating) so by this logic 
+// we will use slow and fast pointer and find out the circle and when we get starting of the cycle so that value is repeating
+
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 // Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
